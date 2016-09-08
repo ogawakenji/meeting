45565fb530d4dcdd3825c5995a5040b85ed18a43
kenoga dev <dev.kenoga.owg@gmail.com>
2016-09-08 23:31:50 +0900

    コミット

--- SeleniumSampleProject/SeleniumSampleProject/Form1.cs
+++ SeleniumSampleProject/SeleniumSampleProject/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 using OpenQA.Selenium;
 using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 
 
@@ -25,17 +26,79 @@ namespace SeleniumSampleProject
         private void button1_Click(object sender, EventArgs e)
         {
             // IEの場合 すべてのゾーンで保護モードを有効にするチェックONに設定する必要がある。
-            InternetExplorerDriver webDriver = new InternetExplorerDriver();
+            InternetExplorerOptions ieOptions = new InternetExplorerOptions();
+            ieOptions.IgnoreZoomLevel = true;
+
+            InternetExplorerDriver webDriver = new InternetExplorerDriver(ieOptions);
 
             webDriver.Navigate().GoToUrl("https://ogawakenji.github.io/meeting/");
 
             var wait = new WebDriverWait(webDriver,TimeSpan.FromSeconds(10));
+
+            SeleniumUtil.browserScale(webDriver);
+            SeleniumUtil.browserScale(webDriver);
+
+
+            SeleniumUtil.elementSendKeysById(webDriver, "txtid1", "abcdefg");
+
+            SeleniumUtil.elementSendKeysById(webDriver, "passid1", "1234567890");
+
+            SeleniumUtil.elementSendKeysById(webDriver, "textareaid1", "abcdefg" + Environment.NewLine + "hijklmn");
+
+            SeleniumUtil.elementSelectTextById(webDriver, "dropdownid1", "ドロップ２");
+
+            SeleniumUtil.browserScaleDown(webDriver);
+            SeleniumUtil.browserScaleDown(webDriver);
+            SeleniumUtil.browserScaleDown(webDriver);
+            SeleniumUtil.browserScaleDown(webDriver);
+
+            SeleniumUtil.browserScaleClear(webDriver);
+
 
             System.Threading.Thread.Sleep(5000);
 
             webDriver.Quit();
 
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            ChromeOptions chOptions = new ChromeOptions();
+            ChromeDriver webDriver = new ChromeDriver(chOptions);
+
+
+            webDriver.Navigate().GoToUrl("https://ogawakenji.github.io/meeting/");
+
+            var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+
+            webDriver.FindElementById("txtid1").SendKeys("abcdefg");
+
+
+            //SeleniumUtil.browserScale(webDriver);
+            //SeleniumUtil.browserScale(webDriver);
+
+
+            //SeleniumUtil.elementSendKeysById(webDriver, "txtid1", "abcdefg");
+
+            //SeleniumUtil.elementSendKeysById(webDriver, "passid1", "1234567890");
+
+            //SeleniumUtil.elementSendKeysById(webDriver, "textareaid1", "abcdefg" + Environment.NewLine + "hijklmn");
+
+            //SeleniumUtil.elementSelectTextById(webDriver, "dropdownid1", "ドロップ２");
+
+            //SeleniumUtil.browserScaleDown(webDriver);
+            //SeleniumUtil.browserScaleDown(webDriver);
+            //SeleniumUtil.browserScaleDown(webDriver);
+            //SeleniumUtil.browserScaleDown(webDriver);
+
+            //SeleniumUtil.browserScaleClear(webDriver);
+
+
+            System.Threading.Thread.Sleep(5000);
+
+            webDriver.Quit();
         }
     }
 }
---
+++ SeleniumSampleProject/SeleniumSampleProject/SeleniumUtil.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace SeleniumSampleProject
+{
+    class SeleniumUtil
+    {
+        #region Idで要素検索し操作
+
+        public static void elementClickById(InternetExplorerDriver driver, string id)
+        {
+            driver.FindElementById(id).Click();
+        }
+
+        public static void elementSendKeysById(InternetExplorerDriver driver, string id,string text)
+        {
+            driver.FindElementById(id).SendKeys(text);
+        }
+
+        public static void elementSelectTextById(InternetExplorerDriver driver, string id ,string text )
+        {
+            IWebElement element = driver.FindElementById(id);
+            SelectElement dropdown = new SelectElement(element);
+            dropdown.SelectByText(text);
+
+        }
+
+
+
+        #endregion
+
+        public static void browserScale(InternetExplorerDriver driver)
+        {
+            IWebElement element = driver.FindElement(By.TagName("html"));
+            element.SendKeys(OpenQA.Selenium.Keys.Control + "+");
+        }
+
+        public static void browserScaleDown(InternetExplorerDriver driver)
+        {
+            IWebElement element = driver.FindElement(By.TagName("html"));
+            element.SendKeys(OpenQA.Selenium.Keys.Control + "-");
+        }
+
+        public static void browserScaleClear(InternetExplorerDriver driver)
+        {
+            // IEのCtrl+0のデフォルトが125%の場合 JavaScriptを埋め込んで100%にする
+            IWebElement element = driver.FindElement(By.TagName("html"));
+            element.SendKeys(OpenQA.Selenium.Keys.Control + "0");
+
+            System.Threading.Thread.Sleep(1000);
+
+            ((IJavaScriptExecutor)driver).ExecuteScript("document.body.style.zoom = '100%';");
+
+            System.Threading.Thread.Sleep(1000);
+
+            ((IJavaScriptExecutor)driver).ExecuteScript("document.body.style.zoom = '70%';");
+
+            System.Threading.Thread.Sleep(1000);
+
+            ((IJavaScriptExecutor)driver).ExecuteScript("document.body.style.zoom = '100%';");
+
+        }
+
+    }
+}
