22fa8cecad1b9dae364fbae66264d7663377a4b5
kenoga dev <dev.kenoga.owg@gmail.com>
2016-09-20 23:34:22 +0900

    コミット

---
+++ SeleniumTest/SeleniumTest/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace SeleniumTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "テスト";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(206, 114);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("ＭＳ ゴシック", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SeleniumTest/SeleniumTest/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // InternetExplorerDriver
+            InternetExplorerOptions ieOptions = new InternetExplorerOptions();
+            ieOptions.IgnoreZoomLevel = true;
+            IWebDriver IEDriver = new InternetExplorerDriver(ieOptions);
+
+            Execute(IEDriver);
+
+            // ChromeDriver
+            IWebDriver ChromeDriver = new ChromeDriver();
+
+            Execute(ChromeDriver);
+
+            // FirefoxDriver
+            IWebDriver FirefoxDriver = new FirefoxDriver();
+
+            Execute(FirefoxDriver);
+               
+        }
+
+        private void Execute(IWebDriver driver)
+        {
+
+            //Googleの画像検索にアクセス
+            driver.Navigate().GoToUrl("https://www.google.co.jp/imghp");
+
+            System.Threading.Thread.Sleep(2000); //待機
+
+            //  最大化
+            driver.Manage().Window.Maximize();
+
+            System.Threading.Thread.Sleep(2000); //待機
+
+            // 検索ボックスの要素を取得
+            IWebElement element1 = driver.FindElement(By.Name("q"));
+
+            // 検索ボタンの要素を取得
+            IWebElement element2 = driver.FindElement(By.Name("btnG"));
+
+            // 検索ボックスに値を設定
+            element1.SendKeys("パンケーキ");
+
+            System.Threading.Thread.Sleep(1000); //待機
+
+            // 検索ボタンクリック
+            if (driver is InternetExplorerDriver)
+            {
+                element2.SendKeys(OpenQA.Selenium.Keys.Enter);
+            }
+            else
+            {
+                element2.Click();
+            }
+
+            System.Threading.Thread.Sleep(5000); //待機
+
+            string currentFilepath = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), DateTime.Now.ToString("yyyyMMddhhmmss") + ".jpg");
+
+            ((ITakesScreenshot)driver).GetScreenshot().SaveAsFile(currentFilepath, ImageFormat.Jpeg);
+
+            System.Threading.Thread.Sleep(2000); //待機
+
+            // 終了
+            driver.Quit();
+
+        }
+
+
+    }
+}
